bee952a2fe5ac3b5c2003f695a5fe4e31182b141
thiyasingh <121523853+thiyasingh@users.noreply.github.com>
2023-01-13 21:25:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//print out either the numbers, or words based on multiples.
+/*for(let i=1; i<=20; i++)
+{
+    if (i%3==0)
+    {
+        console.log("Frontend")
+    }
+    if (i%5==0)
+    {
+        console.log("Simplified")
+    }
+    if (i%3==0 && i%5==0)
+    {
+        console.log("Frontend Simplified")
+    }
+    if (i%3!==0 || i%5 !==0)
+    {
+        console.log(i)
+    }
+}*/
+
+// print out each character from the string.
+/*let str ="Frontend Simplified"
+for(let i=0; i<=str.length-1;i++)
+{
+    console.log(str[i])
+}*/
+
+// change colour of button once clicked
+/*function changeButtonToRed(){
+    document.querySelector("button").style.backgroundColor="red"
+}*/
